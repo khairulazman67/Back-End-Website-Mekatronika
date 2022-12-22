bef012b85e610ea10e9d080c179e15c54416f0ec
khairulazman67 <azman.khairul67@gmail.com>
2022-12-22 20:40:38 +0700

    add categories seeder

--- migrations/20221112061849-create-table-categories.js
+++ migrations/20221112061849-create-table-categories.js
@@ -32,5 +32,6 @@ module.exports = {
 
   async down (queryInterface, Sequelize) {
     await queryInterface.dropTable('categories');
+    
   }
 };
---
+++ seeders/20221222132553-categories-seeder.js
@@ -0,0 +1,55 @@
+'use strict';
+const bcrypt = require('bcrypt');
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('categories', [
+      {
+        kategori: 'Berita Terbaru',
+        keterangan: '',
+        created_at : new Date(),
+        updated_at : new Date()
+      },
+      {
+        kategori: 'Tentang',
+        keterangan: '',
+        created_at : new Date(),
+        updated_at : new Date()
+      },
+      {
+        kategori: 'Visi Misi',
+        keterangan: '',
+        created_at : new Date(),
+        updated_at : new Date()
+      },
+      {
+        kategori: 'Tujuan',
+        keterangan: '',
+        created_at : new Date(),
+        updated_at : new Date()
+      },
+      {
+        kategori: 'SK',
+        keterangan: '',
+        created_at : new Date(),
+        updated_at : new Date()
+      },
+      {
+        kategori: 'CPL',
+        keterangan: '',
+        created_at : new Date(),
+        updated_at : new Date()
+      },
+      {
+        kategori: 'Pengumuman',
+        keterangan: '',
+        created_at : new Date(),
+        updated_at : new Date()
+      }
+    ]);
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('categories', null, {});
+  }
+};
